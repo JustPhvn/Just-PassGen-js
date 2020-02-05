192291246bd1bcc154508a1d8ac4419599471d2c
JustPhvn <justicequyphan@gmail.com>
2020-02-05 12:39:13 -0700

    Fixed passgen to allow ignoring of requirements and meet length & if no requirements selected function fails

--- script.js
+++ script.js
@@ -19,13 +19,18 @@ function passCreation() {
     numbers
   );
   console.log(newPassword);
-  writePassword(newPassword);
+  if (newPassword === undefined) {
+    alert(
+      "Password did not have any requirements please create another password."
+    );
+  } else {
+    writePassword(newPassword);
+  }
 }
 // Write password to the #password input
 function writePassword(newPassword) {
   var password = newPassword;
   var passwordText = document.querySelector("#password");
-  console.log(password);
   passwordText.innerHTML = password;
 }
 //Creates password based on requirements
@@ -36,35 +41,52 @@ function genPassword(
   specialChars,
   numbers
 ) {
-  let password = "";
-  for (let i = 0; i < passLength; i++) {
-    let charType = parseInt(Math.random() * 4);
-    if (charType === 0 && lowerLetters == true) {
-      password += String.fromCharCode(97 + Math.floor(Math.random() * 26));
-    }
-    if (charType === 1 && upperLetters == true) {
-      password += String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    }
-    if (charType === 2 && specialChars == true) {
-      let specCharGroup = parseInt(Math.random() * 3);
-      if (specCharGroup === 0) {
-        password += String.fromCharCode(33 + Math.floor(Math.random() * 15));
+  if (
+    lowerLetters === false &&
+    upperLetters === false &&
+    specialChars === false &&
+    numbers === false
+  ) {
+  } else {
+    let password = "";
+    for (let i = 0; i < passLength; i++) {
+      let charType = parseInt(Math.random() * 4);
+      console.log(charType);
+      if (charType === 0 && lowerLetters == true) {
+        password += String.fromCharCode(97 + Math.floor(Math.random() * 26));
+      } else if (charType === 0 && lowerLetters == false) {
+        i--;
       }
-      if (specCharGroup === 1) {
-        password += String.fromCharCode(58 + Math.floor(Math.random() * 7));
+      if (charType === 1 && upperLetters == true) {
+        password += String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      } else if (charType === 1 && upperLetters == false) {
+        i--;
       }
-      if (specCharGroup === 2) {
-        password += String.fromCharCode(91 + Math.floor(Math.random() * 6));
+      if (charType === 2 && specialChars == true) {
+        let specCharGroup = parseInt(Math.random() * 3);
+        if (specCharGroup === 0) {
+          password += String.fromCharCode(33 + Math.floor(Math.random() * 15));
+        }
+        if (specCharGroup === 1) {
+          password += String.fromCharCode(58 + Math.floor(Math.random() * 7));
+        }
+        if (specCharGroup === 2) {
+          password += String.fromCharCode(91 + Math.floor(Math.random() * 6));
+        }
+        if (specCharGroup === 3) {
+          password += String.fromCharCode(123 + Math.floor(Math.random() * 4));
+        }
+      } else if (charType === 2 && specialChars == false) {
+        i--;
       }
-      if (specCharGroup === 3) {
-        password += String.fromCharCode(123 + Math.floor(Math.random() * 4));
+      if (charType === 3 && numbers == true) {
+        password += String.fromCharCode(48 + Math.floor(Math.random() * 10));
+      } else if (charType === 3 && numbers == false) {
+        i--;
       }
     }
-    if (charType === 3 && numbers == true) {
-      password += String.fromCharCode(48 + Math.floor(Math.random() * 10));
-    }
+    return password;
   }
-  return password;
 }
 
 //Length of password prompt - Min 8 char | Max 128 char, checks if length is between 8 & 128
@@ -75,6 +97,7 @@ function passwordLength() {
       "How many charcters would you like your password to be (8 - 128 char): "
     )
   );
+  console.log("Password Length: " + pLength);
   if (typeof pLength === "number" && pLength >= 8 && pLength <= 128) {
     return pLength;
     console.log(passLength);
@@ -88,10 +111,10 @@ function lowercaseLetters() {
   let lLetters = false;
   lLetters = confirm("Would you like lowercase letters in your password?");
   if (lLetters === true) {
-    console.log("true");
+    console.log("Lowercase: True");
     return lLetters;
   } else {
-    console.log("false");
+    console.log("Lowercase: False");
     return lLetters;
   }
 }
@@ -100,10 +123,10 @@ function uppercaseLetters() {
   let uLetters = false;
   uLetters = confirm("Would you like uppercase letters in your password?");
   if (uLetters === true) {
-    console.log("true");
+    console.log("Uppercase: True");
     return uLetters;
   } else {
-    console.log("false");
+    console.log("Uppercase: False");
     return uLetters;
   }
 }
@@ -112,10 +135,10 @@ function wantNumbers() {
   let needNumbers = false;
   needNumbers = confirm("Would you like numbers in your password?");
   if (needNumbers === true) {
-    console.log("true");
+    console.log("Numbers: True");
     return needNumbers;
   } else {
-    console.log("false");
+    console.log("Numbers: False");
     return needNumbers;
   }
 }
@@ -124,10 +147,10 @@ function specialCharacters() {
   let sChars = false;
   sChars = confirm("Would you like special characters in your password?");
   if (sChars === true) {
-    console.log("true");
+    console.log("Special Characters: True");
     return sChars;
   } else {
-    console.log("false");
+    console.log("Special Characters: False");
     return sChars;
   }
 }
